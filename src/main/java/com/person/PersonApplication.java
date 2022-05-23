b946ae259e87dae2cf29e273df95deab8acacc6d
TheMrVikas <vikas83499@gmail.com>
2022-05-23 22:43:07 +0530

    comment added

--- src/main/java/com/person/PersonApplication.java
+++ src/main/java/com/person/PersonApplication.java
@@ -2,7 +2,11 @@ package com.person;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-
+/**
+ * 
+ * @author Vikas
+ *
+ */
 @SpringBootApplication
 public class PersonApplication {
 
